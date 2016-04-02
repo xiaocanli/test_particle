f1a2cc822697d5cbdbe83421a5540650d5edfb04
Xiaocan Li <phyxiaolee@gmail.com>
2016-04-02 13:57:05 -0600

    Add scripts to load modules

---
+++ module_gcc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+module load gcc/4.7.2
+module load openmpi
+module load friendly-testing
+module load hdf5-parallel/1.8.13
---
+++ module_intel.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+module load intel/14.0.4
+module load openmpi
+module load friendly-testing
+module load hdf5-parallel/1.8.13

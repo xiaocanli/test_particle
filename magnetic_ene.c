10c41806b2ad9afcf303cac648348ea543b00cd8
Xiaocan Li <phyxiaolee@gmail.com>
2016-04-08 10:54:57 -0600

    Use getemf in emfields.h

--- magnetic_ene.c
+++ magnetic_ene.c
@@ -4,20 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
-/* Magnetic fields structure */
-typedef struct bfields {
-	double Bx, By, Bz;
-} bfields;
-
-void getb_ff(double x, double y, double z, double t, struct bfields *bmf);
+#include "wlcs.h"
+#include "emfields.h"
 
 int main(int argc, char *argv[])
 {
     int i, j, k;
     double lmax, dxyz, x, y, z;
     double bene = 0.0;
-    struct bfields *bmf = (bfields *)malloc(sizeof(struct bfields));
+    struct emfields *emf = (emfields *)malloc(sizeof(struct emfields));
     int nmax = 256;
     lmax = 20.0;
     dxyz = lmax / nmax;
@@ -41,9 +36,9 @@ int main(int argc, char *argv[])
             y = dxyz * j;
             for (k=0; k<nmax; k++) {
                 z = dxyz * k;
-                getb_ff(x, y, z, 0.0, bmf);
-                bamp = sqrt(bmf->Bx*bmf->Bx + 
-                        bmf->By*bmf->By + bmf->Bz*bmf->Bz);
+                get_emf(x, y, z, 0.0, emf);
+                bamp = sqrt(emf->Bx*emf->Bx +
+                        emf->By*emf->By + emf->Bz*emf->Bz);
                 ibin = floor((bamp-bmin)/db);
                 b_dist[ibin]++;
                 //if (bamp > bmax) {
@@ -57,37 +52,17 @@ int main(int argc, char *argv[])
     bene = bene/nmax/nmax/nmax;
     printf("Averaged magnetic field energy density: %lf\n", bene);
     FILE *fp;
-    fp = fopen("bene.dat", "w");
+    fp = fopen("data/bene.dat", "w");
     fprintf(fp, "%lf\n", bene);
     fclose(fp);
 
-    fp = fopen("bdist.dat", "w");
+    fp = fopen("data/bdist.dat", "w");
     for (i=0; i<nbins; i++) {
         fprintf(fp, "%lf %d\n", barray[i], b_dist[i]);
     }
     fclose(fp);
-    free(bmf);
+    free(emf);
     free(b_dist);
     free(barray);
     return 0;
 }
-
-void getb_ff(double x, double y, double z, double t, struct bfields *bmf)
-{
-    double B0, lambda0;
-    double ffA, ffB, ffC;
-    B0 = 1.0;
-    lambda0 = 1.0;
-    ffA = 1.0;
-    ffB = sqrt(2.0/3.0);
-    ffC = sqrt(1.0/3.0);
-
-    bmf->Bx = ffA*sin(lambda0*z) + ffC*cos(lambda0*y);
-    bmf->By = ffB*sin(lambda0*x) + ffA*cos(lambda0*z);
-    bmf->Bz = ffC*sin(lambda0*y) + ffB*cos(lambda0*x);
-
-    bmf->Bx *= B0;
-    bmf->By *= B0;
-    bmf->Bz *= B0;
-}
-
